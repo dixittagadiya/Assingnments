c7c63f02ad673ae5b5fdeaff1a932fd0671a2917
dixittagadiya <139606709+dixittagadiya@users.noreply.github.com>
2023-08-02 08:40:26 +0530

    Add files via upload

---
+++ Module 1-1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+    int a;
+
+    printf("Name:- Dixit \n");
+    printf("Birth date:- 18-09-2005 \n");
+    printf("Age:- 18 \n");
+    printf("Address:- 206,Krusnakunj soc.,Nana varacha \n");
+    scanf("%d",&a);
+
+}
