856ec947de6ef188ad2eee3a171f4cfaeb4f0408
dixittagadiya <139606709+dixittagadiya@users.noreply.github.com>
2023-08-02 09:50:48 +0530

    Add files via upload

--- Module 3-3.c
+++ Module 3-3.c
@@ -1,31 +1,50 @@
-#include <stdio.h>
-#include <conio.h>
+#include<stdio.h>
+#include<conio.h>
 void main()
 {
     int a,b,c,d,e,total,per;
+    printf("Enter the Mark blow in \n");
 
-    printf("Enter the mark blow in\n");
-    
     printf("Gujarati:-");
     scanf("%d",&a);
 
-    printf("Hindi:-");
+    printf("English:-");
     scanf("%d",&b);
 
-    printf("Mathe:-");
+    printf("Hindi:-");
     scanf("%d",&c);
 
-    printf("Economic:-");
+    printf("Math:-");
     scanf("%d",&d);
 
-    printf("English:-");
-    scanf("%d",&e); 
+    printf("B.A:-");
+    scanf("%d",&e);
 
-    total= a+ b + c + d + e;
+    total = a + b + c + d + e;
+    printf("\ntotal is %d out of 500",total);
 
-    printf("\ntotal in %d out of 500 ", total);
+    per=total/5;
+    printf("\npercent is %d", per);
 
-    per= total / 5;
-
-    printf("\n percent is %d", per);
+    if(per>=75)
+    {
+        printf("\nDistriction");
+    }
+    else if(per>=60 && per<=75)
+    {
+        printf("\nFirst Class");
+    }
+    else if(per>=50 && per<=60)
+    {
+        printf("\nSecond Class");
+    }
+    else if(per>=35 && per<=50)
+    {
+        printf("\nPass Class");
+    }
+    else
+    {
+        printf("\nFail");
+    }
+    getch();
 }
